3de8a5bfead2aff0b0b02ace3a5b05b38bdf893b
PalanK1N <gromko.sasha@gmail.com>
2021-06-04 15:23:58 +0200

    Add files via upload

---
+++ Lab3Minecraft/README.md
@@ -0,0 +1,3 @@
+Hromko Oleksandr KV-82 Lab3
+
+https://docs.google.com/document/d/1E36xV4fj__laLtfFyHwtxcrmiJ5PrpLyDt9yA9LbfjM/edit?usp=sharing
---
+++ Lab3Minecraft/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>
+      Hello worlds
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
